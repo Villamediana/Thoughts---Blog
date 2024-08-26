bdd355925729750686066311edf6b361a7f9eaef
Villamediana <miguel.villamediana@outlook.com>
2024-08-26 13:14:07 -0300

    Second commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import os
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Heroku!"
+    return render_template('home.html')
+
 
 if __name__ == "__main__":
     app.run()
-
